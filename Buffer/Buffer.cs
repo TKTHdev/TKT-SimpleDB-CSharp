1ac2e64a7edaee041df14769379aa650b2d4c1f5
TKTHdev <tkt0430@keio.jp>
2026-03-11 14:25:36 +0900

    buffer empty impl

--- Buffer/Buffer.cs
+++ Buffer/Buffer.cs
@@ -1,2 +1,15 @@
+using DBSharp.File;
+using DBSharp.Log;
+using DBSharp.Buffer;
+
 namespace DBSharp.Buffer;
 
+public class Buffer
+{
+    public Buffer(FileMgr fm, LogMgr lm){}
+    public Page contents(){return null;}
+    public BlockId block(){return null;}
+    public bool isPinned(){return false;}
+    public void setModified(int txnum, int lsn){}
+    public int modifyingTx(){return 0;}
+}
--- Buffer/BufferMgr.cs
+++ Buffer/BufferMgr.cs
@@ -1,2 +1,13 @@
+using DBSharp.File;
+using DBSharp.Log;
+
 namespace DBSharp.Buffer;
 
+public class BufferMgr
+{
+    public BufferMgr(FileMgr fm, LogMgr lm, int numbuffs){}
+    public Buffer pin(BlockId blk){return null;}
+    public void unpin(BlockId buff){}
+    public int available(){return 0;}
+    public void flushAll(int txnum){}
+}
