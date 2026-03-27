a1237a58a2b78ac24e3e95c14df9401d2bd0e6ec
TKTHdev <tkt0430@keio.jp>
2026-03-27 10:56:20 +0900

    recovery mgr

---
+++ Log/RecoveryMgr.cs
@@ -0,0 +1,35 @@
+﻿using System.Transactions;
+using DBSharp.Buffers;
+using Buffer = DBSharp.Buffers.Buffer;
+
+namespace DBSharp.Log;
+
+public class RecoveryMgr
+{
+    public RecoveryMgr(Transaction tx, int txnum, LogMgr lm, BufferMgr bm)
+    {
+    }
+
+    public void commit()
+    {
+    }
+
+    public void rollback()
+    {
+    }
+
+    public void recover()
+    {
+    }
+
+    public int setInt(Buffer buff, int offset, int newval)
+    {
+        return -1;
+    }
+
+    public int setString(Buffer buff, int offset, String newval)
+    {
+        return -1;
+    }
+
+}
