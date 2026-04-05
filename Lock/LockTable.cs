e352b81411c26347e77a2712ffdcb5d2aa18dfc5
TKTHdev <tkt0430@keio.jp>
2026-04-05 15:35:02 +0900

    locktable skeleton

---
+++ Lock/LockTable.cs
@@ -0,0 +1,47 @@
+﻿using DBSharp.File;
+
+namespace DBSharp.Lock;
+
+public class LockTable
+{
+   private static readonly long MAX_TIME = 10000; // 10 seconds
+   private Dictionary<BlockId, int>locks =  new Dictionary<BlockId, int>();
+
+   public void SLock(BlockId blk)
+   {
+      lock(this)
+      {}
+   }
+   public void XLock(BlockId blk)
+   {
+      lock(this)
+      {}
+   }
+
+   public void Unlock(BlockId blk)
+   {
+      lock (this)
+      {
+         
+      }
+   }
+
+   private bool hasXLock(BlockId blk)
+   {
+      return true;
+   }
+   private bool hasOtherSLocks(BlockId blk)
+   {
+      return true;
+   }
+
+   private bool waitingTooLong(long startTime)
+   {
+      return true;
+   }
+
+   private int getLockVal(BlockId blk)
+   {
+      return 0;
+   }
+}
