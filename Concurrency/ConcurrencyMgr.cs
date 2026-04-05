864f6c7ee9db245aae70bad3f689586a5fdb68a8
TKTHdev <tkt0430@keio.jp>
2026-04-05 16:29:04 +0900

    ConcurrencyMgr skeleton

---
+++ Concurrency/ConcurrencyMgr.cs
@@ -0,0 +1,29 @@
+﻿using DBSharp.File;
+
+namespace DBSharp.Lock;
+
+public class ConcurrencyMgr
+{
+    private static LockTable locktbl  = new LockTable();
+    private Dictionary<BlockId, string> locks = new Dictionary<BlockId, string>();
+
+    public void slock(BlockId blk)
+    {
+    }
+
+    public void xlock(BlockId blk)
+    {
+        
+    }
+
+    public void release()
+    {
+        
+    }
+
+    private bool hasXLock(BlockId blk)
+    {
+        string locktype = locks[blk];
+        return locktype != null && locktype.Equals("X");
+    }
+}
