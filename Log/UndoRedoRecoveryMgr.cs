d0a43d4acc0348a6da300f5c5698e64d4b825bc9
TKTHdev <tkt0430@keio.jp>
2026-04-10 16:36:13 +0900

    Constructor

--- Log/UndoRedoRecoveryMgr.cs
+++ Log/UndoRedoRecoveryMgr.cs
@@ -1,9 +1,23 @@
-﻿using Buffer = DBSharp.Buffers.Buffer;
-
+﻿using DBSharp.Buffers;
+using Buffer = DBSharp.Buffers.Buffer;
+using DBSharp.Transactions;
 namespace DBSharp.Log;
 
 public class UndoRedoRecoveryMgr : IRecoveryMgr
 {
+    private LogMgr _lm;
+    private IBufferMgr _bm;
+    private Transaction _tx;
+    private int _txnum;
+
+    public  UndoRedoRecoveryMgr(Transaction tx, int txnum, LogMgr lm, IBufferMgr bm)
+    {
+       _tx = tx;
+       _txnum = txnum;
+       _lm = lm;
+       _bm = bm;
+       StartRecord.WriteToLog(lm, txnum);
+    }
     public void Commit()
     {
         throw new NotImplementedException();
