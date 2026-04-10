bfcdc1fdc78685a258cc6de6524526a1a6c17716
TKTHdev <tkt0430@keio.jp>
2026-04-10 16:11:46 +0900

    UndoRedoRecoveryMgr.cs skeleton

---
+++ Log/UndoRedoRecoveryMgr.cs
@@ -0,0 +1,36 @@
+﻿using Buffer = DBSharp.Buffers.Buffer;
+
+namespace DBSharp.Log;
+
+public class UndoRedoRecoveryMgr : IRecoveryMgr
+{
+    public void Commit()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Rollback()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Recover()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SetInt(Buffer buff, int offset, int newval)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SetString(Buffer buff, int offset, string newval)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Append(string filename)
+    {
+        throw new NotImplementedException();
+    }
+}
