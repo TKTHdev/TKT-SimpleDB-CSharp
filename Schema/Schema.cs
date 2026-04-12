faec645f9241497b025beb014d82b969193612df
TKTHdev <tkt0430@keio.jp>
2026-04-12 12:28:51 +0900

    Schema.cs skeleton

---
+++ Schema/Schema.cs
@@ -0,0 +1,63 @@
+﻿using System.Reflection;
+
+namespace DBSharp.Schema;
+
+public class Schema
+{
+    private List<string> fields = new List<string>();
+    private Dictionary<string, FieldInfo> info = new  Dictionary<string, FieldInfo>();
+
+    public void AddField(string fieldname, int type, int length)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddIntField(string fieldname)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddStringField(string fieldname, int length)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(string fieldname, Schema sch)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddAll(Schema sch)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<string> Fields()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool HasField(string fieldname)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Type(string fieldname)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Length(string fieldname)
+    {
+        throw new NotImplementedException();
+    }
+    class FieldInfo
+    {
+        int type, length;
+        public FieldInfo(int type, int length)
+        {
+            this.type = type;
+            this.length = length;
+        }
+    }
+}
