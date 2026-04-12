7bf235de8a366ccbae12a3e68fa43eb6c31a0fd0
TKTHdev <tkt0430@keio.jp>
2026-04-12 13:32:04 +0900

    Layout.cs skeleton

---
+++ Schema/Layout.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Schema;
+
+namespace DBSharp.Schema;
+
+public class Layout
+{
+    private Schema schema;
+    private Dictionary<string, int> offsets;
+    private int slotsize;
+
+    public Layout(Schema schema)
+    {
+        this.schema = schema;
+        offsets = new Dictionary<string, int>();
+        int pos = sizeof(int); // space for the empty/inuse flag
+        foreach (string fieldname in schema.Fields())
+        {
+           offsets.Add(fieldname, pos);
+           pos += lengthInBytes(fieldname);
+        }
+        slotsize = pos;
+    }
+
+    public Layout(Schema schema, Dictionary<string, int> offsets, int slotsize)
+    {
+        this.schema = schema;
+        this.offsets = offsets;
+        this.slotsize = slotsize;
+    }
+
+    public Schema GetSchema()
+    {
+        return schema;
+    }
+
+    public int GetOffset(string fieldname)
+    {
+        // maybe need to be modified 
+        // since it might not be handled properly when offsets[fieldname] = null?
+        return offsets[fieldname];
+    }
+
+    public int GetSlotSize()
+    {
+        return slotsize;
+    }
+    
+
+    private int lengthInBytes(string fieldname)
+    {
+        throw new NotImplementedException();
+    }
+}
