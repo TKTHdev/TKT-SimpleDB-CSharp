4ec2e18b2c464bdb514d7c3fae5059b177c490f2
TKTHdev <tkt0430@keio.jp>
2026-04-18 17:31:09 +0900

    UpdateScan now extend Scan

--- Scan/UpdateScan.cs
+++ Scan/UpdateScan.cs
@@ -1,6 +1,15 @@
+using DBSharp.Record;
+
 namespace DBSharp.Scan;
+using DBSharp.TmpClass;
 
-public interface UpdateScan
+public interface IUpdateScan: IScan
 {
-
+    public void SetInt(string fidname, int val);
+    public void SetString(string fldname, string val);
+    public void SetVal(string fldname, Constant val);
+    public void Insert();
+    public void Delete();
+    public RID GetRid();
+    public void MoveToRid(RID rid);
 }
