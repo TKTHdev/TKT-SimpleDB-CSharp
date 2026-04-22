b85e752288426e8db052474a5120378d3a27597c
TKTHdev <tkt0430@keio.jp>
2026-04-22 16:41:25 +0900

    Expression

---
+++ Predicate/Expression.cs
@@ -0,0 +1,50 @@
+﻿namespace DBSharp.Predicate;
+using DBSharp.Scan;
+using DBSharp.Record;
+
+public class Expression
+{
+   private Constant val = null;
+   private string fldname = null;
+
+   public Expression(Constant val)
+   {
+      this.val = val;
+   }
+
+   public Expression(string fldname)
+   {
+      this.fldname = fldname;
+   }
+
+   public bool IsFieldName()
+   {
+      return fldname != null;
+   }
+
+   public Constant AsConstant()
+   {
+      return val;
+   }
+
+   public string AsFieldName()
+   {
+      return fldname;
+   }
+
+   public Constant Evaluate(IScan s)
+   {
+      return (val != null) ? val : s.GetVal(fldname);
+   }
+
+   public bool AppliesTo(Schema sch)
+   {
+      return (val != null)? true : sch.HasField(fldname);
+   }
+
+   public String ToString()
+   {
+      return (val !=null) ?  val.ToString() : fldname;
+   }
+   
+}
