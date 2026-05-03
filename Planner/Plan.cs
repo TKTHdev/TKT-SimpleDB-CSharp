41ab76095af948797a90fdfa731bc90a7f3672d5
TKTHdev <tkt0430@keio.jp>
2026-05-03 14:57:19 +0900

    Plan interface

--- Planner/Plan.cs
+++ Planner/Plan.cs
@@ -1,6 +1,12 @@
 ﻿namespace DBSharp.Planner;
+using DBSharp.Scan;
+using DBSharp.Record;
 
-public class Plan
+public interface Plan
 {
-
+    public IScan open();
+    public int BlockAccessed();
+    public int RecordsOutput();
+    public int DistinctValues(string fldname);
+    public Schema Schema();
 }
