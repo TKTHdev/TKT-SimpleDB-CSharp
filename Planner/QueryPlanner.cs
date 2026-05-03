bc58ad65932b77209f0a5ee136f91e778265078a
TKTHdev <tkt0430@keio.jp>
2026-05-03 18:45:13 +0900

    QueryPlanner interface

---
+++ Planner/QueryPlanner.cs
@@ -0,0 +1,9 @@
+﻿using DBSharp.Parser;
+using DBSharp.Transactions;
+
+namespace DBSharp.Planner;
+
+public interface QueryPlanner
+{
+    public IPlan CreatePlan(QueryData data, Transaction tx);
+}
